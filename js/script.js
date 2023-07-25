c9ee993ed252f430ee7a4a8d5274e1f30230bf24
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-25 15:56:40 +0300

    Display cloase icon and remove menu icon

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 
 const menuIcon = document.querySelector('.hamburger-icon');
 const nav = document.querySelector('.desktop-nav');
+const close = document.querySelector('.close-icon');
 
 menuIcon.addEventListener('click', function () {
   nav.classList.add('nav');
+  close.style.display = 'block';
+  menuIcon.style.display = 'none';
 });
