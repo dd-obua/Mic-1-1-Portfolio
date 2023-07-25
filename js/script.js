0647b539ace17aba36c3036fe48883e715f7ff0b
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-25 18:59:29 +0300

    Add functionality of clicking menu options

--- js/script.js
+++ js/script.js
@@ -4,14 +4,30 @@ const menuIcon = document.querySelector('.hamburger-icon');
 const nav = document.querySelector('.desktop-nav');
 const close = document.querySelector('.close-icon');
 
+const portfolio = document.querySelector('#portfolio');
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
+
 menuIcon.addEventListener('click', function () {
   nav.classList.add('nav');
   close.style.display = 'block';
   menuIcon.style.display = 'none';
 });
 
+portfolio.addEventListener('click', () => {
+  nav.classList.remove('nav');
+});
+
+about.addEventListener('click', () => {
+  nav.classList.remove('nav');
+});
+
+contact.addEventListener('click', () => {
+  nav.classList.remove('nav');
+});
+
 close.addEventListener('click', () => {
-  nav.classList.remove('nav')
+  nav.classList.remove('nav');
   close.style.display = 'none';
   menuIcon.style.display = 'block';
-})
+});
