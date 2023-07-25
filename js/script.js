17707b7ddcce9d212221b9754cc6b34b6709205c
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-25 20:07:49 +0300

    Refactor code for showing mobile menu

--- js/script.js
+++ js/script.js
@@ -3,31 +3,13 @@
 const menuIcon = document.querySelector('.hamburger-icon');
 const nav = document.querySelector('.desktop-nav');
 const close = document.querySelector('.close-icon');
+const menuItems = document.querySelectorAll('.desktop-nav>li');
 
-const portfolio = document.querySelector('#portfolio');
-const about = document.querySelector('#about');
-const contact = document.querySelector('#contact');
-
-menuIcon.addEventListener('click', function () {
+const showMobileMenu = function () {
   nav.classList.add('nav');
   close.style.display = 'block';
   menuIcon.style.display = 'none';
-});
+};
 
-portfolio.addEventListener('click', () => {
-  nav.classList.remove('nav');
-});
-
-about.addEventListener('click', () => {
-  nav.classList.remove('nav');
-});
-
-contact.addEventListener('click', () => {
-  nav.classList.remove('nav');
-});
-
-close.addEventListener('click', () => {
-  nav.classList.remove('nav');
-  close.style.display = 'none';
-  menuIcon.style.display = 'block';
-});
+menuIcon.addEventListener('click', showMobileMenu);
+    
