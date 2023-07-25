a24ff5c16ddd9791d600b8e22b844a6eb66eb791
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-25 21:34:42 +0300

    Refactor script.js file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
-'use strict';
-
 const menuIcon = document.querySelector('.hamburger-icon');
 const nav = document.querySelector('.desktop-nav');
 const close = document.querySelector('.close-icon');
 const menuItems = document.querySelectorAll('.desktop-nav>li');
 
-const showMobileMenu = function () {
+const showMobileMenu = () => {
   nav.classList.add('nav');
   close.style.display = 'block';
   menuIcon.style.display = 'none';
 };
 
-const hideMobileMenu = function () {
+const hideMobileMenu = () => {
   nav.classList.remove('nav');
   menuIcon.style.display = 'block';
 };
@@ -20,4 +18,4 @@ menuIcon.addEventListener('click', showMobileMenu);
 
 close.addEventListener('click', hideMobileMenu);
 
-menuItems.forEach(item => item.addEventListener('click', hideMobileMenu));
+menuItems.forEach((item) => item.addEventListener('click', hideMobileMenu));
