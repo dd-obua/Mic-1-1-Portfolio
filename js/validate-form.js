3035a9f0b91f4f42966b1421ca46732d708935c6
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-26 13:31:54 +0300

    Refactor the event handler using arrow function

--- js/validate-form.js
+++ js/validate-form.js
@@ -2,7 +2,7 @@ const emailInput = document.querySelector('.email');
 const submitBtn = document.querySelector('.submit-form');
 const errorMsg = document.querySelector('.email-error');
 
-submitBtn.addEventListener('click', function (event) {
+submitBtn.addEventListener('click', (event) => {
   if (emailInput.value !== emailInput.value.toLowerCase()) {
     event.preventDefault();
     errorMsg.style.display = 'block';
