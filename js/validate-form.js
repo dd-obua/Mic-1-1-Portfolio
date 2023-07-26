0dc7e161ced0b20890cbdc99ca5b9d4fba36dcf7
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-26 13:28:21 +0300

    Prevent form submission if email is not in lowercase

--- js/validate-form.js
+++ js/validate-form.js
@@ -3,4 +3,8 @@ const submitBtn = document.querySelector('.submit-form');
 const errorMsg = document.querySelector('.email-error');
 
 submitBtn.addEventListener('click', function (event) {
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    event.preventDefault();
+    errorMsg.style.display = 'block';
+  }
 });
