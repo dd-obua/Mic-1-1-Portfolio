ec702500f3585bae29541be69929b96743c69fc7
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-27 18:33:35 +0300

    Move card HTML inside generateCards function

--- js/popup.js
+++ js/popup.js
@@ -24,23 +24,25 @@ const workCards = [
   }
 ];
 
-const cardHtml = `
-  <h3>${card.name1} <span class="gain-glory">${card.name2}</span></h3>
-
-  <ul class='works__categories'>
-    <li class='works__skill'>${card.technologies[0]}</li>
-    <li class='works__skill'>${card.technologies[1]}</li>
-    <li class='works__skill'>${card.technologies[2]}</li>
-    <li class='works__skill'>${card.technologies[3]}</li>
-  </ul>
-
-  <a href="#" class="link see-project">See Project</a>
-`;
-
 const generateCards = () => {
+  const cardHtml = `
+    <h3>${card.name1} <span class="gain-glory">${card.name2}</span></h3>
+
+    <ul class='works__categories'>
+      <li class='works__skill'>${card.technologies[0]}</li>
+      <li class='works__skill'>${card.technologies[1]}</li>
+      <li class='works__skill'>${card.technologies[2]}</li>
+      <li class='works__skill'>${card.technologies[3]}</li>
+    </ul>
+
+    <a href="#" class="link see-project">See Project</a>
+  `;
+
   workCards.forEach((card) => {
     const card = document.createElement("artcile");
     card.className = "works__card";
     card.innerHtml = cardHtml;
   });
 };
+
+const generateMobilePopup = () => {};
