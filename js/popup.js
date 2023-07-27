fa22b620bd93bb4984bee6c6fed990bf4bf429a6
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-27 18:16:00 +0300

    Create function for generating cards

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,20 @@
+const works = document.querySelector(".works");
+
 const workCards = [
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  },
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  },
   {
     name1: "Multi-Post Stories",
     name2: "Gain+Glory",
@@ -7,3 +23,10 @@ const workCards = [
     sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
   }
 ];
+
+const generateCards = () => {
+  workCards.forEach((card) => {
+    const card = document.createElement("artcile");
+    card.className = "works__card";
+  });
+};
