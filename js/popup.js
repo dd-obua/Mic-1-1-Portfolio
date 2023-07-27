6ceb583e9817c437e5878c249b484860ca7b5fa2
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-27 17:53:52 +0300

    Create works object

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+const works = [
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  }
+];
