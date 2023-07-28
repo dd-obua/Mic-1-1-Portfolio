12284cc37a00554d643472f20b24685af54c4aca
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-28 17:09:09 +0300

    Fix errors in popup.js file

--- js/popup.js
+++ js/popup.js
@@ -148,6 +148,7 @@ const createDesktopPopup = () => {
       </ul>
       <p>${card.description1} ${card.description2}</p>
     `;
+
     const desktopPopupElement = document.createElement('section');
     desktopPopupElement.className = 'popup-desktop';
     desktopPopupElement.innerHTML = desktopPopupHtml;
