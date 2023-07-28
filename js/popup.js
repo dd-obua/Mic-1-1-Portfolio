249deac9c7f2f75023bad31ab5dc0db567785a3e
Obua Denis Daniel <dd.obua@gmail.com>
2023-07-28 12:26:57 +0300

    Update cards object

--- js/popup.js
+++ js/popup.js
@@ -13,6 +13,61 @@ const cards = [
     technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
     liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
     sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  },
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    description1:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    description2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  },
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    description1:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    description2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  },
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    description1:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    description2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  },
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    description1:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    description2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
+  },
+  {
+    name1: "Multi-Post Stories",
+    name2: "Gain+Glory",
+    description1:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    description2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    liveVersionLink: "https://dd-obua.github.io/Mic-Portfolio/",
+    sourceLink: "https://github.com/dd-obua/Mic-Portfolio"
   }
 ];
 
